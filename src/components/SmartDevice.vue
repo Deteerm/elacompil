f08a23f000b982d41405060e9787b3ef4d5dcf09
Dominik Tarkiewicz <dominik.tarkiewicz@gmail.com>
2022-02-19 20:33:20 +0100

    basic list with simple grid

---
+++ src/components/SmartDevice.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="smart-device-root">
+    <div>type: {{ this.id }}</div>
+    <div>name</div>
+    <div>connectionState</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String, // 'bulb', 'outlet' or 'temperatureSensor';
+    id: String,
+    name: String,
+    connectionState: String
+  }
+}
+</script>
+
+<style scoped>
+.smart-device-root {
+  border: 1px solid black;
+  padding: 10px;
+  width: fit-content;
+}
+</style>
